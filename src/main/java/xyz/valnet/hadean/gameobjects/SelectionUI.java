d10bf0900bedab390c3d1abc1af0b5e1089af7c8
Valerie <valerie@valnet.xyz>
2022-05-21 01:22:43 -0400

    create selection narrowing buttons

--- src/main/java/xyz/valnet/hadean/gameobjects/SelectionUI.java
+++ src/main/java/xyz/valnet/hadean/gameobjects/SelectionUI.java
@@ -5,12 +5,16 @@ import java.util.HashMap;
 import java.util.List;
 
 import xyz.valnet.engine.scenegraph.GameObject;
+import xyz.valnet.hadean.input.Button;
+import xyz.valnet.hadean.input.IButtonListener;
 import xyz.valnet.hadean.util.Assets;
 
-public class SelectionUI extends GameObject implements ISelectionChangeListener {
+public class SelectionUI extends GameObject implements ISelectionChangeListener, IButtonListener {
 
   private List<ISelectable> selected = new ArrayList<ISelectable>();
   private HashMap<String, Integer> selectedTypes = new HashMap<String, Integer>();
+  private HashMap<String, Button> narrowButtons = new HashMap<String, Button>();
+  private HashMap<Button, List<ISelectable>> narrowBuckets = new HashMap<Button, List<ISelectable>>();
 
   private Selection selectionManager;
 
@@ -24,26 +28,60 @@ public class SelectionUI extends GameObject implements ISelectionChangeListener 
 
     Assets.uiFrame.draw(10, 366, 300, 200);
 
-    int i = 0;
-    for(String name : selectedTypes.keySet()) {
-      int n = selectedTypes.get(name);
-      Assets.font.drawString("" + n + "x " + name, 26, 376 + 16 * i);
-      i ++;
+    // int i = 0;
+    // for(String name : selectedTypes.keySet()) {
+    //   int n = selectedTypes.get(name);
+    //   Assets.font.drawString("" + n + "x " + name, 26, 376 + 16 * i);
+    //   i ++;
+    // }
+
+    for(Button btn : narrowButtons.values()) {
+      btn.draw();
+    }
+  }
+
+  @Override
+  public void tick(float dTime) {
+    for(Button btn : narrowButtons.values()) {
+      btn.update();
     }
   }
 
   @Override
   public void selectionChanged(List<ISelectable> selected) {
     this.selected = selected;
-    
+
     selectedTypes.clear();
+    narrowButtons.clear();
+    narrowBuckets.clear();
     for(ISelectable selectable : selected) {
       String name = selectable.getClass().getName();
+      System.out.println(name);
+      String[] splitName = name.split("\\.");
+      for(String s : splitName) {
+        System.out.println(s);
+      }
+      String shortName = splitName[splitName.length - 1];
+      
       if(selectedTypes.containsKey(name)) {
         selectedTypes.replace(name, selectedTypes.get(name) + 1);
+        Button btn = narrowButtons.get(name);
+        List<ISelectable> items = narrowBuckets.get(btn);
+        items.add(selectable);
       } else {
+        Button btn = new Button(Assets.uiFrame, shortName, 20, 376 + 30 * selectedTypes.size(), 280, 24);
+        btn.registerClickListener(this);
         selectedTypes.put(name, 1);
+        narrowButtons.put(name, btn);
+        List<ISelectable> list = new ArrayList<ISelectable>();
+        list.add(selectable);
+        narrowBuckets.put(btn, list);
       }
     }
+  }
+
+  @Override
+  public void click(Button target) {
+    
   }
 }

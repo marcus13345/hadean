bec5cca553123b0114143bdb6be757f1bd397b8c
Valerie <valerie@valnet.xyz>
2022-05-21 06:32:35 -0400

    util for last commit. whoops.

---
+++ src/main/java/xyz/valnet/engine/math/Vector2i.java
@@ -0,0 +1,17 @@
+package xyz.valnet.engine.math;
+
+public class Vector2i {
+
+	public int x, y;
+
+	public Vector2i() {
+		x = 0;
+		y = 0;
+	}
+
+	public Vector2i(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}

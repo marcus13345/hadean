cf77644f4b06796e04b9c25789f2438745f8770f
Ivory <valerie@valnet.xyz>
2023-01-03 01:11:18 -0500

    width in world objects, bed stubbed

---
+++ src/main/java/xyz/valnet/hadean/gameobjects/worldobjects/Bed.java
@@ -0,0 +1,26 @@
+package xyz.valnet.hadean.gameobjects.worldobjects;
+
+import xyz.valnet.engine.math.Vector4f;
+import xyz.valnet.hadean.interfaces.IBuildable;
+
+public class Bed extends WorldObject implements IBuildable {
+
+  @Override
+  public void buildAt(int x, int y, int w, int h) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public String getName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Vector4f getWorldBox() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+}

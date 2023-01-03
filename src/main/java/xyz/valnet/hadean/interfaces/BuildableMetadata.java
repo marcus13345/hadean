a10f9bf25ff9acbcd3dad91c1d2a442dfe60ffbc
Bronwen <marcus@valnet.xyz>
2023-01-03 02:17:43 -0500

    buildtab: added buildable types

--- src/main/java/xyz/valnet/hadean/interfaces/BuildableMetadata.java
+++ src/main/java/xyz/valnet/hadean/interfaces/BuildableMetadata.java
@@ -5,6 +5,11 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BuildableMetadata {
+
+  public static int AREA = 0;
+  public static int SINGLE = 1;
+
   public String name();
   public String category();
+  public int type() default AREA;
 }

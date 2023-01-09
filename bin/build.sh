32693d8a2b4f60717ef0883165062145ecffd330
Bronwen <marcus@valnet.xyz>
2023-01-09 04:32:59 -0500

    macos change, works now

--- bin/build.sh
+++ bin/build.sh
@@ -9,6 +9,7 @@ java -jar ./bin/packr-all-4.0.0.jar ./bin/packr.windows.json
 
 echo " ==== Macos ==== "
 java -jar ./bin/packr-all-4.0.0.jar ./bin/packr.macos.json
+mv output/macos/built output/macos/Hadean.app
 
 echo " ==== Linux ==== "
 java -jar ./bin/packr-all-4.0.0.jar ./bin/packr.linux.json
